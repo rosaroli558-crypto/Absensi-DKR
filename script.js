aa871b5ee876931443f36a8db30246b06a48281b
rosaroli558-crypto <rosaroli558@gmail.com>
2026-02-17 00:56:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+console.log("JS jalan");
+
 // 🔥 Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC88eNtWMuOQ4eezVriirq_sjjVOkfl8K8",
