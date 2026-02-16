14d86e196396c23548e18f17fe464369ae393401
rosaroli558-crypto <rosaroli558@gmail.com>
2026-02-16 22:27:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC88eNtWMuOQ4eezVriirq_sjjVOkfl8K8",
+  authDomain: "absensi-dkr.firebaseapp.com",
+  databaseURL: "https://absensi-dkr-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "absensi-dkr",
+  storageBucket: "absensi-dkr.firebasestorage.app",
+  messagingSenderId: "824325578551",
+  appId: "1:824325578551:web:3fa855eab199686e5d84b2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.absen = function() {
+  const nama = document.getElementById("nama").value;
+
+  if (nama === "") {
+    alert("Nama wajib diisi");
+    return;
+  }
+
+  push(ref(db, "absensi/"), {
+    nama: nama,
+    waktu: new Date().toLocaleString()
+  });
+
+  alert("Berhasil absen!");
+}
