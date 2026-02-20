016e63bd659ff3ebbc8e03c28910737d2255b125
rosaroli558-crypto <rosaroli558@gmail.com>
2026-02-21 04:43:26 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-eimport { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getDatabase,
   ref,
