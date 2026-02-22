948f1a207020c8ac03069b0d284a33f424c201ac
rosaroli558-crypto <rosaroli558@gmail.com>
2026-02-23 01:39:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
   getDatabase,
   ref,
@@ -7,7 +7,7 @@ import {
   get,
   query,
   limitToLast
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
