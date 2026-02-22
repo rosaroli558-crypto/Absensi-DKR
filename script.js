6cd1a271dfb880c2bdb0eca93be6bd923b4f00bf
rosaroli558-crypto <rosaroli558@gmail.com>
2026-02-23 02:32:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ import {
   limitToLast
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 /* ================= FIREBASE CONFIG ================= */
 
